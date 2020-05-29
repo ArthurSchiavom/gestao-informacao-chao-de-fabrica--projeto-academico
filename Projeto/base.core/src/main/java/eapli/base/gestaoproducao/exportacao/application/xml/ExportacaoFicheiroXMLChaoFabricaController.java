67cadf668b15f56bbb65863feaef384993ee32c8
Metriximor <pedro.pikacho@gmail.com>
2020-05-29 04:44:23 +0100

    UC-I-39: pequeno bugfix

--- Projeto/base.core/src/main/java/eapli/base/gestaoproducao/exportacao/application/xml/ExportacaoFicheiroXMLChaoFabricaController.java
+++ Projeto/base.core/src/main/java/eapli/base/gestaoproducao/exportacao/application/xml/ExportacaoFicheiroXMLChaoFabricaController.java
@@ -2,10 +2,13 @@ package eapli.base.gestaoproducao.exportacao.application.xml;
 
 import eapli.base.gestaoproducao.exportacao.application.FormatoExportacao;
 import eapli.base.gestaoproducao.exportacao.domain.ServicoExportacao;
+import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.infrastructure.persistence.RepositoryFactory;
 
 import java.util.Date;
 
 public class ExportacaoFicheiroXMLChaoFabricaController {
+	private final RepositoryFactory repoFact = PersistenceContext.repositories();
 	private final ServicoExportacao servicoExportacao = new ServicoExportacao(repoFact);
 
 	public boolean exportar(String path, boolean exportarTempoProd, boolean exportarDesvios, Date dataAFiltrar) {
