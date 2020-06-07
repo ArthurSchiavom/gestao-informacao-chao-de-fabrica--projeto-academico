10b9fc1b0931ec9fb996f9556c092430a09f9391
DiogoAlmeida <1161752@isep.ipp.pt>
2020-06-07 05:40:25 +0100

    UC-I-060: Partially implemented

--- Projeto/base.core/src/main/java/eapli/base/tcp/TcpSrvRecolherMensagensGeradasPelasMaquinas.java
+++ Projeto/base.core/src/main/java/eapli/base/tcp/TcpSrvRecolherMensagensGeradasPelasMaquinas.java
@@ -1,13 +1,19 @@
 package eapli.base.tcp;
 
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
 import java.io.IOException;
 import java.net.*;
 
 public class TcpSrvRecolherMensagensGeradasPelasMaquinas implements Runnable {
 
     public static final int PORT_NUMBER = 6834;
+    static final String TRUSTED_STORE="server_J.jks";
+    static final String KEYSTORE_PASS="forgotten";
 
-    static ServerSocket sock;
+//    static ServerSocket sock;
+    static SSLServerSocket sock=null;
+
 
     @Override
     public void run() {
@@ -18,12 +24,24 @@ public class TcpSrvRecolherMensagensGeradasPelasMaquinas implements Runnable {
                 System.out.println("thread: "+ip);
             }
 
+            // Trust these certificates provided by authorized clients
+            System.setProperty("javax.net.ssl.trustStore", TRUSTED_STORE);
+            System.setProperty("javax.net.ssl.trustStorePassword",KEYSTORE_PASS);
+
+            // Use this certificate and private key as server certificate
+            System.setProperty("javax.net.ssl.keyStore",TRUSTED_STORE);
+            System.setProperty("javax.net.ssl.keyStorePassword",KEYSTORE_PASS);
+
+            SSLServerSocketFactory sslF = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+
             Socket cliSock;
 
             try {
-                sock = new ServerSocket(PORT_NUMBER);
+//                sock = new ServerSocket(PORT_NUMBER);
+                sock = (SSLServerSocket) sslF.createServerSocket(PORT_NUMBER);
+                sock.setNeedClientAuth(true);
             } catch (IOException ex) {
-                System.out.println("Failed to open server socket");
+                System.out.println("Failed to open server socket " + ex.getMessage());
                 System.exit(1);
             }
 
